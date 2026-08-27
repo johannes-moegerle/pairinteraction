2315f09ea2207b695510016f12dee23a338f0a4b
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2026-08-27 15:28:59 +0200

    [C++] KetAtom add has_quantum_number

--- src/cpp/include/pairinteraction/ket/KetAtom.hpp
+++ src/cpp/include/pairinteraction/ket/KetAtom.hpp
@@ -30,6 +30,7 @@ public:
     Database &get_database() const;
     size_t get_id_in_database() const;
     const std::string &get_species() const;
+    bool has_quantum_number(const std::string &name) const;
     double get_quantum_number(const std::string &name) const;
     double get_quantum_number_std(const std::string &name) const;
 
